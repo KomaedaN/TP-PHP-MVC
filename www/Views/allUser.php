6cd6b3ae8c835df3253f983b6aba718ff16586e5
Thomas <84379061+KomaedaN@users.noreply.github.com>
2025-12-10 10:49:09 +0100

    les utilisateurs ne peuvent plus supprimer les autres sans être admin

--- www/Views/allUser.php
+++ www/Views/allUser.php
@@ -10,11 +10,11 @@
             <p><?= $user['email'] ?></p>
             <p>id :<?= $user['id'] ?></p>
             <p>admin :<?= $user['is_admin'] ? 'true' : 'false' ?></p>
-            <form action="/deleteUser" method="POST">
+            <?php if(isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == true && $user['is_admin'] == false): ?>
+                <form action="/deleteUser" method="POST">
                 <input type="hidden" name="id" value="<?= $user['id'] ?>">
                 <input class="btn btn_red" type="submit" value="Supprimer">
             </form>
-            <?php if(isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == true && $user['is_admin'] == false): ?>
              <form action="/setAdmin" method="POST">
                 <input type="hidden" name="id" value="<?= $user['id'] ?>">
                 <input class="btn btn_red" type="submit" value="Mettre Admin">
